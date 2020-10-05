06a47d4a2be8a428d78490c02d6f949912dfa6ce
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-05 15:02:46 -0300

    Add request/response to the new route

--- src/Controller/OlaMundoController.php
+++ src/Controller/OlaMundoController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class OlaMundoController
 {
     /**
      * @Route("/ola")
      */
-    public function olaMundoAction()
+    public function olaMundoAction(Request $request): Response
     {
-        echo "Ola mundo!";
-        exit();
+        $pathInfo  = $request->getPathInfo();
+        // $parametro = $request->query->get('parametro');
+        $parametro = $request->get('parametro');
+        $query = $request->query->all();
+        return new JsonResponse([
+            'mensagem' => 'Ola mundo!',
+            'pathInfo' => $pathInfo,
+            'parametro' => $parametro,
+            'query' => $query]);
     }
 }
