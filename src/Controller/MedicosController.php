88221d4426e0549245940f1dc9445e72c6a24e39
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-05 20:36:21 -0300

    Add delete endpoint

--- src/Controller/MedicosController.php
+++ src/Controller/MedicosController.php
@@ -85,6 +85,18 @@ class MedicosController extends AbstractController
         return new JsonResponse($medicoExistente);
     }
 
+    /**
+     * @Route("/medicos/{id}", methods={"DELETE"})
+     */
+    public function remove(int $id): Response
+    {
+        $medico = $this->buscaMedico($id);
+        $this->entityManager->remove($medico);
+        $this->entityManager->flush();
+
+        return new Response(null, Response::HTTP_NO_CONTENT);
+    }
+
     public function buscaMedico(int $id)
     {
         $repositorioDeMedicos = $this->getDoctrine()->getRepository(Medico::class);
