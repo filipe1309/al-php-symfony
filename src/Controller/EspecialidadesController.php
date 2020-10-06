24606c042c0c8a378304dc70fef2ead2efaf529d
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-06 13:44:24 -0300

    Implement Update and Delete Especialidade

--- src/Controller/EspecialidadesController.php
+++ src/Controller/EspecialidadesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Especialidade;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\EspecialidadeRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,10 +14,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class EspecialidadesController extends AbstractController
 {
     private $entityManager;
+    private $repository;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, EspecialidadeRepository $repository)
     {
         $this->entityManager = $entityManager;
+        $this->repository = $repository;
     }
 
     /**
@@ -35,4 +38,53 @@ class EspecialidadesController extends AbstractController
 
         return new JsonResponse($especialidade);
     }
+
+    /**
+     * @Route("/especialidades", methods={"GET"})
+     */
+    public function buscarTodas(): Response
+    {
+        $especialidadeList = $this->repository->findAll();
+
+        return new JsonResponse($especialidadeList);
+    }
+
+    /**
+     * @Route("/especialidades/{id}", methods={"GET"})
+     */
+    public function buscarUma(int $id): Response
+    {
+        $especialidade = $this->repository->find($id);
+
+        return new JsonResponse($especialidade);
+    }
+
+    /**
+     * @Route("/especialidades/{id}", methods={"PUT"})
+     */
+    public function atualiza($id, Request $request): Response
+    {
+        $dadosRequest = $request->getContent();
+        $dadosEmJson = json_decode($dadosRequest);
+
+        $especialidade = $this->repository->find($id);
+        $especialidade->setDescricao($dadosEmJson->descricao);
+
+        $this->entityManager->flush();
+
+        return new JsonResponse($especialidade);
+    }
+
+    /**
+     * @Route("/especialidades/{id}", methods={"DELETE"})
+     */
+    public function remove($id): Response
+    {
+        $especialidade = $this->repository->find($id);
+        $this->entityManager->remove($especialidade);
+        $this->entityManager->flush();
+
+        return new Response(null, Response::HTTP_NO_CONTENT);
+    }
+      
 }
