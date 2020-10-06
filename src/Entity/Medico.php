e4c9eab32055667454a1a461b23b8314fee7eb09
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-06 13:24:23 -0300

    Add especialidade to Medico Factory and Entity

--- src/Entity/Medico.php
+++ src/Entity/Medico.php
@@ -3,32 +3,62 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use JsonSerializable;
 
 /**
  * @ORM\Entity()
  */
-class Medico
+class Medico implements JsonSerializable
 {
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    public $id;
+    private $id;
     /**
      * @ORM\Column(type="integer")
      */
-    public $crm;
+    private $crm;
     /**
      * @ORM\Column(type="string")
      */
-    public $nome;
+    private $nome;
 
     /**
      * @ORM\ManyToOne(targetEntity=Especialidade::class)
      * @ORM\JoinColumn(nullable=false)
      */
     private $especialidade;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCrm(): ?int
+    {
+        return $this->crm;
+    }
+
+    public function setCrm(int $crm): self
+    {
+        $this->crm = $crm;
+
+        return $this;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
 
     public function getEspecialidade(): ?Especialidade
     {
@@ -40,5 +70,15 @@ class Medico
         $this->especialidade = $especialidade;
 
         return $this;
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            'id' => $this->getId(),
+            'crm' => $this->getCrm(),
+            'nome' => $this->getNome(),
+            'especialidadeId' => $this->getEspecialidade()->getId(),
+        ];
     }
 }
