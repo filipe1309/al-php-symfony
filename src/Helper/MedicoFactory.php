b163d62c984bc9a2a0e11df795357de71dbb916f
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-08 10:48:05 -0300

    Add Entity Guard clauses

--- src/Helper/MedicoFactory.php
+++ src/Helper/MedicoFactory.php
@@ -17,9 +17,9 @@ class MedicoFactory implements EntidadeFactory
     public function criarEntidade(string $json): Medico
     {
         $dadoEmJson = json_decode($json);
-        if(!property_exists($dadoEmJson, 'nome') || !property_exists($dadoEmJson, 'crm') || !property_exists($dadoEmJson, 'especialidadeId')) {
-            throw new EntityFactoryException('Medico precisa de nome, crm e especialidade'); 
-        }
+        // Guard clauses
+        $this->checkAllProperties($dadoEmJson);
+
         $especialidadeId = $dadoEmJson->especialidadeId;
         $especialidade = $this->especialidadeRepository->find($especialidadeId);
 
@@ -30,5 +30,20 @@ class MedicoFactory implements EntidadeFactory
             ->setEspecialidade($especialidade);
 
         return $medico;
+    }
+
+    private function checkAllProperties($dadoEmJson): void
+    {
+        if(!property_exists($dadoEmJson, 'nome')) {
+            throw new EntityFactoryException('Medico precisa de nome'); 
+        }
+        
+        if(!property_exists($dadoEmJson, 'crm')) {
+            throw new EntityFactoryException('Medico precisa de crm'); 
+        }
+
+        if(!property_exists($dadoEmJson, 'especialidadeId')) {
+            throw new EntityFactoryException('Medico precisa de especialidade'); 
+        }
     }
 }
