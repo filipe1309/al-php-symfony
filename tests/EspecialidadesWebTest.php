b79cc1bec2b2af6685ebc8c99435d0fd2bb53d62
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-10-08 18:15:48 -0300

    Add test with authentication

--- tests/EspecialidadesWebTest.php
+++ tests/EspecialidadesWebTest.php
@@ -13,4 +13,44 @@ class EspecialidadesWebTest extends WebTestCase
 
         self::assertEquals(401, $client->getResponse()->getStatusCode());
     }
+
+    public function testGaranteQueEspecialidadesSaoListadas()
+    {
+        $client = static::createClient();
+        $token = $this->login($client);
+        $client->request('GET', '/especialidades', [], [], [
+            'HTTP_AUTHORIZATION' => "Bearer $token"
+        ]);
+
+        $reposta = json_decode($client->getResponse()->getContent());
+
+        self::assertTrue($reposta->sucesso);
+        self::assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testInsereEspecialidade()
+    {
+        $client = static::createClient();
+        $token = $this->login($client);
+        $client->request('POST', '/especialidades', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+            'HTTP_AUTHORIZATION' => "Bearer $token"
+        ], json_encode([
+            'descricao' => 'Especialidade Teste'
+        ]));
+
+        self::assertEquals(201, $client->getResponse()->getStatusCode());
+    }
+
+    private function login($client): string
+    {
+        $client->request('POST', '/login', [], [], [
+            'CONTENT_TYPE' => 'application/json'
+        ], json_encode([
+            'usuario' => 'usuario',
+            'senha' => '123456'
+        ]));
+        
+        return json_decode($client->getResponse()->getContent())->access_token;
+    }
 }
